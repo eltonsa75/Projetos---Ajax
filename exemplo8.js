6262a5ae28938f506db78433400212cec958dc67
eltonsa75 <eltonsa75@hotmail.com>
2017-06-22 17:07:11 -0300

    Requisição do HTTP com XML
    Requisição do HTTP com XML

---
+++ exemplo8.js
@@ -0,0 +1,17 @@
+
+window.onload = function(){
+	var palavra = document.getElementById("palavra");
+	if(palavra != null)
+		palavra.onblur = function() { traduz(palavra.value); }
+}
+function traduz(palavra){
+	if(palavra){
+		var url="exemplo8.php?palavra="+encodeURIComponent(palavra);
+		requisicaoHTTP("GET" ,url,true);
+	}
+}
+function trataDados(){
+	var info = ajax.responseText; 	//obtém a resposta como string
+	var resposta = document.getElementById("traducao");
+	resposta.value = info;
+}
